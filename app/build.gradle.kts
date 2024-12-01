ff70d555bc01cfad5a97f8c009791ba1d0db7174
sbtsdump2024 <russelfour.ortanez@gmail.com>
2024-12-01 18:27:52 +0800

    v1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,16 @@ android {
     namespace = "com.finals.centric"
     compileSdk = 34
 
+    lint {
+        baseline = file("lint-baseline.xml")
+    }
+
     defaultConfig {
         applicationId = "com.finals.centric"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -46,8 +50,6 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.glide)
     implementation(libs.annotation)
-    implementation(libs.lifecycle.livedata.ktx)
-    implementation(libs.lifecycle.viewmodel.ktx)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
